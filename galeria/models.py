cd7461dae6c87a134ed83d9daf44327b05318688
Wanjiru393 <carolciru393@gmail.com>
2022-05-31 23:41:22 +0300

    Created Location model

--- galeria/models.py
+++ galeria/models.py
@@ -8,7 +8,16 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Location(models.Model):
+    location = models.CharField(max_length=100,blank=False, null=False)
+
+    def __str__(self):
+        return self.location
+
+
 class Photo(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
     description = models.TextField()
