7ed4d2e58855ad0bcb2671f9ffcedc4f65a943d1
Wanjiru393 <carolciru393@gmail.com>
2022-06-01 08:19:37 +0300

    Created tests

--- galeria/tests.py
+++ galeria/tests.py
@@ -1,3 +1,80 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.test import TestCase
+
+from .models import Category, Image, Location
+# Create your tests here.
+
+class LocationTestCLass(TestCase):
+    #Set up Method
+    def setUp(self):
+        self.loc = Location(name="Maldives")
+        self.loc.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loc,Location))
+
+    def test_save_method(self):
+        self.loc.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    
+ #test for Category Model
+
+class CategoryTestClass(TestCase):
+    # Set up Method
+    def setUp(self):
+        self.cat = Category(name="fashion")
+        self.cat.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cat, Category))
+
+    def test_save_method(self):
+        self.cat.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+    
+
+ #test for Image Model
+
+class ImageTestClass(TestCase):
+    # Set up Method
+    def setUp(self):
+        self.cat = Category(name="fashion")
+        self.cat.save_category()
+
+        self.loc = Location(name="Maldives")
+        self.loc.save_location()
+
+        self.image = Image(name='image test', description='my test',image_location=self.loc, image_category=self.cat)
+        self.image.save_image()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    
+    def test_save_method(self):
+        self.image.save_image()
+        images  = Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+
+    def test_get_all_images(self):
+        images = Image.get_all_images()
+        self.assertTrue(len(images)>0)
+
+    def test_get_image_by_id(self):
+        images= Image.get_image_by_id(self.image.id)
+        self.assertTrue(len(images) == 1)
+
+    def test_search(self):
+        images = Image.search_by_category('fashion')
+        self.assertTrue(len(images)>0)
+
+
+    
