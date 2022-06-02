4e496faed4f6f337d357c9dec59c502dae14f6a3
Wanjiru393 <carolciru393@gmail.com>
2022-06-02 11:43:01 +0300

    deployment

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default =False,cast=bool)
+# DEBUG = config('DEBUG', default =False,cast=bool)
+
+DEBUG = os.environ.get('DEBUG', False)
 
 # development
 if config('MODE')=="dev":
